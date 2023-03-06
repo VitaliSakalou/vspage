ebec204ac612b5a7c05dd7aa643a131e382bceda
Vitali Sakalou <vitali.sakalou@gmail.com>
2023-03-05 19:55:46 -0800

    mobile menu top improvements

--- components/MobileNavbar.tsx
+++ components/MobileNavbar.tsx
@@ -26,12 +26,12 @@ const MobileNavbar = ({
 			} fixed flex z-50  bottom-0 backdrop-blur-[8px] backdrop-brightness-75 justify-end transition-left duration-[500ms]  w-full`}
 		>
 			<div
-				className="bg-white p-6 w-[80%] h-[100vh]"
+				className="bg-white p-6 pt-16 w-[80%] h-[100vh]"
 				onClick={(e) => {
 					e.stopPropagation();
 				}}
 			>
-				<nav className="flex justify-center mt-[80px]">
+				<nav className="flex justify-center">
 					<ul className="inline-flex justify-center flex-col items-center">
 						{navigation.map(({ id, title, path }) => {
 							const linkStyle = classNames('my-8', {
